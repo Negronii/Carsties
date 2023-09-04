45752eb74f4a248bfbbc3018b92fc34a88e36350
Negronii <478258625@qq.com>
2023-09-05 08:50:00 +1000

    first day

---
+++ src/AuctionService/Data/AuctionDbContext.cs
@@ -0,0 +1,13 @@
+using AuctionServices.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionServices.Data;
+
+public class AuctionDbContext : DbContext {
+    
+    public AuctionDbContext(DbContextOptions options) : base(options) {
+        
+    }
+
+    public DbSet<Auction> Auctions { get; set; }
+}
---
+++ src/AuctionService/Entities/Status.cs
@@ -0,0 +1,9 @@
+namespace AuctionServices.Entities
+{
+    public enum Status
+    {
+        Live, 
+        Finished, 
+        ReserveNotMet,
+    }
+}
